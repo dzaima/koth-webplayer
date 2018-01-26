37b6c51991636ebe90f5cc1c89fb2f46b710bcbd
dzaima <atdzim@inbox.lv>
2018-01-26 16:23:55 +0200

    make new antries show always (in contrast of not ever showing)

--- games/formic/Display.js
+++ games/formic/Display.js
@@ -392,7 +392,7 @@ define([
 						}
 					}
 					let className = '';
-					let displayMe = displayTeamList.includes(ant.entry);
+					let displayMe = displayTeamList.includes(ant.entry) || ant.entry[0] === "N";
 					if(ant.type === QUEEN) {
 						if(this.queenMarkerType && displayMe) {
 							className = 'queen-locator-' + this.queenMarkerType;
